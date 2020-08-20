8f0b039f79172e2797e081844e8440d3d781d0ec
nizi <failaforcontact@gmail.com>
2020-08-20 21:37:14 +0900

    [add] CellularPhone.javaを追加

---
+++ CellularPhone.java
@@ -0,0 +1,8 @@
+public class CellularPhone {
+  String telephoneNumber = "123-4567";
+
+  public void receiveCall() {
+    System.out.println("ring! ring! ring!");
+    System.out.println("This is" + telephoneNumber);
+  }
+}
